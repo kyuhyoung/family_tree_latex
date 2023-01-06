d321e1ad42d7614528088d40959a35ed777f57bf
Kyuhyoung Choi <kyuhyoung@gmail.com>
2023-01-06 22:10:17 +0900

    Add the case of multiple wives.

--- usage.sh
+++ usage.sh
@@ -1,3 +1,15 @@
-#: << 'END'
+: << 'END'
+#	Gauss
 cd gauss; pdflatex ft_gauss.tex; cd -
+END
+: << 'END'
+#	mutiple wives test
+aidi=multi_wives
+cd ${aidi}; pdflatex ${aidi}.tex; cd -
+END
+#: << 'END'
+#	Chosun dyansty
+aidi=chosun_dynasty
+cd ${aidi}; pdflatex ${aidi}.tex; cd -
 #END
+
